34e4251ed44b772755f4c03bd2ba4bdded73faaf
YSB_uncyclocity <seongbeom_lee@kakao.com>
2022-01-06 09:03:04 +0900

    [UPDATE] 이진 탐색 로직 수정
    배열에 값이 없으면 null로 뜨도록 설정

--- 탐색/이진탐색.js
+++ 탐색/이진탐색.js
@@ -3,16 +3,18 @@ const search = (arr, target) => {
   let high = arr.length - 1;
   let mid = parseInt((low + high) / 2);
 
-  while (arr[mid] !== target) {
+  while (low <= high) {
     if (arr[mid] < target) {
       low = mid + 1;
     } else if (arr[mid] > target) {
       high = mid - 1;
+    } else {
+      return mid;
     }
     mid = parseInt((low + high) / 2);
   }
 
-  return mid;
+  return null;
 };
 
-console.log(search([1, 2, 4, 6, 8, 9, 10], 2));
+console.log(search([1, 2, 4, 6, 8, 9, 10], 7));
